27823c9763fa8d4e299847a15d1f00f730040744
AwesomeAlexander <alexander.m.ng@gmail.com>
2020-10-30 14:39:21 -0700

    Sketchy keyboard

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,37 +1,104 @@
-import p5 = require("p5");
+import * as p5 from "p5";
 
-// The midi notes of a scale
-var notes;
 var osc;
+let curMousedKey: Key;
+let curPressedKeys: Key[];
+
+// Button
+// class Button {
+//   constructor(x,y,width,height) {
+    
+//   }
+
+  
+// }
 
 // A key
-type Key = {
+interface Key {
   midiFreq: number,
   x: number,
   y: number,
   width: number,
   height: number,
+  keyCode?: number,
   isBlack?: boolean
 }
-type Keyboard = { keys: Key[], x: number, y: number, width: number, height: number };
-var keyboard: Keyboard;
 
 /**
- * Tests whether a point is within a rectangular region.
- * @param x the x coord to be considered
- * @param y the y coord to be considered
- * @param testX the lower x bound
- * @param testY the lower y bound
- * @param testWidth the width (such that x+width is the upper x bound)
- * @param testHeight the height (such that y+height is the upper y bound)
+ * Manual constructor bc this is bad
+ * @param midiFreq midi frequency code
+ * @param x x coord
+ * @param y y coord
+ * @param width width of key
+ * @param height height of key
+ * @param keyCode computer keycode
+ * @param isBlack whether the key is black
  */
-function isWithin(x, y, testX, testY, testWidth, testHeight) {
-  return x >= testX && x <= testX + testWidth
-    && y >= testY && y <= testY + testHeight;
+function makeKey(midiFreq: number, x: number, y: number, width: number, height: number, keyCode?: number, isBlack?: boolean): Key {
+  return {
+    midiFreq: midiFreq,
+    x:  x,
+    y:  y,
+    width: width,
+    height: height,
+    keyCode: keyCode,
+    isBlack: isBlack
+  };
+}
+  
+/**
+ * Tests whether a coordinate point is within this object's bounds
+ * @param pointX the x coord to test
+ * @param pointY the y coord to test
+ */
+function isWithin(kb: Keyboard, key: Key, pointX: number, pointY: number): boolean {
+  return pointX >= (key.x+kb.x) && pointX <= (key.x+kb.x) + key.width
+      && pointY >= (key.y+kb.y) && pointY <= (key.y+kb.y) + key.height;
 }
 
+interface Keyboard {
+  keys: Key[],
+  x: number,
+  y: number,
+  width: number,
+  height: number
+
+  // constructor(/*keys: Key[], */x: number, y: number, width: number, height: number) {
+  //   this.keys = [];
+  //   this.x =x;
+  //   this.y = y;
+  //   this.width = width;
+  //   this.height = height;
+  // }
+
+  // drawKey(key: Key, filler?) {
+  //   if (!!filler) fill(filler);
+  //   rect(key.x + this.x, key.y + this.y, key.width, key.height);
+  // }
+};
+var keyboard: Keyboard;
+
+function drawKey(keyboard:Keyboard, key: Key, filler?) {
+  if (!!filler) fill(filler);
+  rect(key.x + keyboard.x, key.y + keyboard.y, key.width, key.height);
+}
+
+// /** 
+//  * Tests whether a point is within a rectangular region.
+//  * @param x the x coord to be considered
+//  * @param y the y coord to be considered
+//  * @param testX the lower x bound
+//  * @param testY the lower y bound
+//  * @param testWidth the width (such that x+width is the upper x bound)
+//  * @param testHeight the height (such that y+height is the upper y bound)
+//  */
+// function isWithin(x, y, testX, testY, testWidth, testHeight) {
+//   return x >= testX && x <= testX + testWidth
+//       && y >= testY && y <= testY + testHeight;
+// }
+
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(1200, 400);
   stroke("black");
 
   // A triangle oscillator
@@ -40,39 +107,66 @@ function setup() {
   osc.start();
   osc.amp(0);
   // Defining the keyboard
-  keyboard = { keys: [], x: 0, y: 0, width: windowWidth, height: windowHeight };
+  // keyboard = new Keyboard(20, 40, 800, 300);
+  keyboard = { keys: [], x: 20, y: 40, width: 800, height: 300 };
+  curPressedKeys = [];
+  curMousedKey = null;
 
   // white keys
   let w = keyboard.width / 8;
-  keyboard.keys.push({ midiFreq: 60, x: 0 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 62, x: 1 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 64, x: 2 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 65, x: 3 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 67, x: 4 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 69, x: 5 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 71, x: 6 * w, y: 0, width: w, height: keyboard.height });
-  keyboard.keys.push({ midiFreq: 72, x: 7 * w, y: 0, width: w, height: keyboard.height });
+  keyboard.keys.push(makeKey(60, 0 * w, 0, w, keyboard.height,  65)); // press a
+  keyboard.keys.push(makeKey(62, 1 * w, 0, w, keyboard.height,  83)); // press s
+  keyboard.keys.push(makeKey(64, 2 * w, 0, w, keyboard.height,  68)); // press d
+  keyboard.keys.push(makeKey(65, 3 * w, 0, w, keyboard.height,  70)); // press f
+  keyboard.keys.push(makeKey(67, 4 * w, 0, w, keyboard.height,  74)); // press j
+  keyboard.keys.push(makeKey(69, 5 * w, 0, w, keyboard.height,  75)); // press k
+  keyboard.keys.push(makeKey(71, 6 * w, 0, w, keyboard.height,  76)); // press l
+  keyboard.keys.push(makeKey(72, 7 * w, 0, w, keyboard.height, 186)); // press ;
 
   // black keys
-  let black_w = w * 0.4; // TODO fix
-  keyboard.keys.push({ midiFreq: 61, x: 80, y: 0, width: black_w, height: keyboard.height / 2, isBlack: true });
-  keyboard.keys.push({ midiFreq: 63, x: 180, y: 0, width: black_w, height: keyboard.height / 2, isBlack: true });
-  keyboard.keys.push({ midiFreq: 66, x: 380, y: 0, width: black_w, height: keyboard.height / 2, isBlack: true });
-  keyboard.keys.push({ midiFreq: 68, x: 480, y: 0, width: black_w, height: keyboard.height / 2, isBlack: true });
-  keyboard.keys.push({ midiFreq: 70, x: 580, y: 0, width: black_w, height: keyboard.height / 2, isBlack: true });
+  let bw = w * 0.4; // TODO unhardcode x
+  let bx = w - bw/2;
+  keyboard.keys.push(makeKey(61, bx, 0, bw, keyboard.height * 0.6, 87, true));
+  keyboard.keys.push(makeKey(63, 180, 0, bw, keyboard.height * 0.6, 69, true ));
+  keyboard.keys.push(makeKey(66, 380, 0, bw, keyboard.height * 0.6, 85, true ));
+  keyboard.keys.push(makeKey(68, 480, 0, bw, keyboard.height * 0.6, 73, true ));
+  keyboard.keys.push(makeKey(70, 580, 0, bw, keyboard.height * 0.6, 79, true ));
 }
 
 function draw() {
   // Draw a keyboard
-  // keyboard.keys.forEach(k => {
+  let mouseOnBlack = false;
+  curMousedKey = null;
   for (let k of keyboard.keys) {
-    fill(200);
-    if (isWithin(mouseX, mouseY, k.x + keyboard.x, k.y + keyboard.y, k.width + keyboard.width, k.height + keyboard.height)) {
-      if (mouseIsPressed) fill(100, 255, 100);
-      else fill(127);
+    // Default unhighlighted colors
+    let filler: any = (k.isBlack) ? 0 : 255;
+    // if (k.isBlack) fill(0);
+    // else fill(255);
+
+    // Mouse
+    if (isWithin(keyboard, k, mouseX, mouseY)) {
+      if (k.isBlack) mouseOnBlack = true;
+      curMousedKey = k;
+      filler = (mouseIsPressed) ? "yellow" : "green";
+      // if (mouseIsPressed) fill("yellow");
+      // else fill("green");
     }
-    rect(k.x + keyboard.x, k.y + keyboard.y, k.width + keyboard.width, k.height + keyboard.height);
-  } //});
+    
+    // Draw
+    // keyboard.drawKey(k);
+    drawKey(keyboard, k, filler);
+    
+    // // Mouse & drawing
+    // if (isWithin(mouseX, mouseY, k.x + keyboard.x, k.y + keyboard.y, k.width, k.height)) {
+    //   if (mouseIsPressed) fill(100, 255, 100); // played
+    //   else fill("yellow"); // highlighted
+    //   highlighted = true;
+    // }
+    // rect(k.x + keyboard.x, k.y + keyboard.y, k.width, k.height);
+  }
+
+  // selected.forEach(k => playNote(k.midiFreq));
+  // selected = [];
 }
 
 // A function to play a note
@@ -89,15 +183,22 @@ function playNote(note, duration?) {
   }
 }
 
+function keyPressed() {
+  keyboard.keys.forEach(k => {
+    if (!!k.keyCode && keyIsDown(k.keyCode)) curPressedKeys.push(k); // add k
+    else curPressedKeys = curPressedKeys.filter(u => u != k); // remove k
+  });
+
+  curPressedKeys.forEach(k => playNote(k.midiFreq));
+}
+
+function keyReleased() {
+  osc.fade(0, 0.5);
+}
+
 // When we click
 function mousePressed() {
-  // Map mouse to the key index
-  for (let k of keyboard.keys.reverse()) {
-    if (isWithin(mouseX, mouseY, k.x + keyboard.x, k.y + keyboard.y, k.width + keyboard.width, k.height + keyboard.height)) {
-      playNote(k.midiFreq);
-      return;
-    }
-  }
+  if (!!curMousedKey) playNote(curMousedKey.midiFreq);
 }
 
 
